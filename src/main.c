407b7dd615c224c0e9d9b84e5228b1cca9c75e3d
da526 <da526@njit.edu>
2024-10-06 23:57:43 -0400

    minimally working window

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+#include "state.h"
+#include <stdlib.h>
+
 int main() {
+    atexit(state_destroy);
+    state_init();
+    state_loop();
     return 0;
 }
--- src/window/window.h
+++ src/window/window.h
@@ -1,4 +1,11 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include <glad.h>
+#include <glfw.h>
+
+typedef struct {
+    GLFWwindow* handle;
+} Window;
+
 #endif
